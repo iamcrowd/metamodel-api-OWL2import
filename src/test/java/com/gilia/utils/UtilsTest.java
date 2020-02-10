ff723f9e22ed523b217c208aae1aa566bf235199
German Braun <gab@gab.gab>
2020-02-10 09:35:28 -0300

    resources and first test for importing owl files

--- src/test/java/com/gilia/utils/UtilsTest.java
+++ src/test/java/com/gilia/utils/UtilsTest.java
@@ -1,6 +1,38 @@
-package com.gilia.utils
+package com.gilia.utils;
 
-import spock.lang.Specification
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-class UtilsTest extends Specification {
+//import static groovy.test.GroovyAssert.*
+
+//import spock.lang.Specification;
+
+import org.apache.commons.io.IOUtils;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static com.gilia.utils.Utils.validateOWL;
+
+/**
+mvn clean test -Dtest=UtilsTest -DfailIfNoTests=false
+*/
+
+public class UtilsTest {
+
+	@Test
+    public void testLoadOWL2FileOK() {
+    	try {
+ //   		String owlfile = new String(UtilsTest.class.getClassLoader().getResource("ontologies/pizza.owl").toString()); 
+    	  	validateOWL("/home/gab/Documentos/KF/metamodelapi-owlimport/target/test-classes/ontologies/pizza.owl");
+    	} 
+    	catch (Exception e){
+        	e.printStackTrace();
+    	}
+
+
+    }
+    
 }
