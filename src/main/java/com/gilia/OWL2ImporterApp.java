1223c7d39f5662ae690f530f6f60004efee2414a
German Braun <gab@gab.gab>
2020-02-10 09:35:44 -0300

    importer app class

---
+++ src/main/java/com/gilia/OWL2ImporterApp.java
@@ -0,0 +1,31 @@
+package com.gilia;
+
+import com.gilia.config.SwaggerConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.gilia.metamodel.*;
+import org.semanticweb.owlapi.*;
+
+/**
+ * API Application Init. The execution of main method of this class results in the building and deployment of the API.
+ */
+ 
+@SpringBootApplication
+@Import(SwaggerConfiguration.class)
+public class OWL2ImporterApp implements WebMvcConfigurer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OWL2ImporterApp.class, args);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+    }
+}
