8331a8d356cf42ad9ed21e9fd8e782d401383fd9
gbraun <gbraun@gilia.gilia>
2020-02-12 14:21:44 -0300

    main importer class

--- src/main/java/com/gilia/owlimporter/importer/Importer.java
+++ src/main/java/com/gilia/owlimporter/importer/Importer.java
@@ -1,1 +1,84 @@
-package com.gilia.owlimporter.importer;
+package com.gilia.owlimporter;
+
+import com.gilia.metamodel.*;
+
+import org.json.simple.JSONObject;
+
+import org.semanticweb.owlapi.io.*;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.util.*;
+import org.semanticweb.owlapi.apibinding.*;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import static com.gilia.utils.ImportUtils.validateOWL;
+
+import com.gilia.exceptions.EmptyOntologyException;
+
+
+/**
+ * An importer is a KF metamodel instance of an OWL 2 specification
+ * 
+ * @author gbraun
+ *
+ */
+public class Importer {
+	
+	private Metamodel kfimported;
+	private OWLOntology onto;
+	private OWLOntologyManager man;
+	
+	/**
+	 * 
+	 * @param filePath
+	 */
+	public Importer(String filePath) {
+		try {
+			File file = new File(filePath);
+			validateOWL(file);
+			this.kfimported = new Metamodel();
+			this.man = OWLManager.createOWLOntologyManager();
+	        this.onto = man.loadOntologyFromOntologyDocument(file);
+		}
+		catch (Exception e){
+        	e.printStackTrace();
+    	}
+	}
+	
+	/**
+	 * 
+	 * @param iri
+	 */
+	public Importer(IRI iri) {
+		try {
+			validateOWL(iri);
+			this.kfimported = new Metamodel();
+			this.man = OWLManager.createOWLOntologyManager();
+	        this.onto = man.loadOntologyFromOntologyDocument(iri);
+		}
+		catch (Exception e){
+        	e.printStackTrace();
+		}
+	}
+	
+	public Metamodel getKFInstance() {
+		return this.kfimported;
+	}
+	
+	public OWLOntologyManager getOntologyManager() {
+		return this.man;
+	}
+	
+	public OWLOntology getOntology() {
+		return this.onto;
+	}
+	
+//	public void addOWLClasses(ArrayList<EntityType> classes) {
+//		this.kfimported.setEntities(classes);
+//	}
+	
+}
---
+++ src/main/java/com/gilia/owlimporter/importer/OWLClasses.java
@@ -0,0 +1,20 @@
+package com.gilia.owlimporter.importer;
+
+import com.gilia.metamodel.entitytype.EntityType;
+import com.gilia.metamodel.entitytype.objecttype.ObjectType;
+
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ *
+ * Import classes from an OWL 2 specification
+ *
+ * @author gbraun
+ *
+ */
+public class OWLClasses {
+	
+}
