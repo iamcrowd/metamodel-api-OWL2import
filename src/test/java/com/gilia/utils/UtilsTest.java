101150db40ad0502d1dd962ea4f020c7639605d8
gbraun <gbraun@gilia.gilia>
2020-02-14 15:53:16 -0300

    test for utils

--- src/test/java/com/gilia/utils/UtilsTest.java
+++ src/test/java/com/gilia/utils/UtilsTest.java
@@ -1,6 +1,7 @@
 package com.gilia.utils;
 
 import org.junit.jupiter.api.Test;
+import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.junit.jupiter.api.*;
 
 import org.apache.commons.io.IOUtils;
@@ -10,10 +11,22 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
+import static com.gilia.utils.ImportUtils.validateOWL;
+
 /**
 mvn clean test -Dtest=UtilsTest -DfailIfNoTests=false
 */
 
 public class UtilsTest {
     
+	@Test
+    public void testValidateOWLFromFile() {
+		try {
+			File file = new File("/var/www/html/metamodelapi-owlimport/src/test/resources/ontologies/gufo.ttl"); 
+			validateOWL(file);
+		}
+		catch (Exception e){
+        	e.printStackTrace();
+		}
+	}
 }
