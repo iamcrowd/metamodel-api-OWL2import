40d9f014a1711cf8a90e0fb38b00570143b14bbb
German Braun <gab@gab.gab>
2020-09-07 15:56:49 -0300

    new function to get a normalised ontology. Now we need to loop over each axioms, identify their types and create the respective KF axioms for each type

--- src/main/java/com/gilia/owlimporter/importer/Importer.java
+++ src/main/java/com/gilia/owlimporter/importer/Importer.java
@@ -6,6 +6,7 @@ import org.json.simple.JSONObject;
 
 import org.semanticweb.owlapi.io.*;
 import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.model.parameters.Imports;
 import org.semanticweb.owlapi.util.*;
 import org.semanticweb.owlapi.apibinding.*;
 
@@ -14,8 +15,12 @@ import org.semanticweb.owlapi.reasoner.InferenceType;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -30,6 +35,13 @@ import com.gilia.owlimporter.importer.axiom.classAxiom.SubClassOf;
 import com.gilia.exceptions.EmptyOntologyException;
 
 
+import www.ontologyutils.toolbox.AnnotateOrigin;
+import www.ontologyutils.toolbox.FreshAtoms;
+import www.ontologyutils.toolbox.Utils;
+import www.ontologyutils.normalization.NormalizationTools;
+import www.ontologyutils.normalization.Normalization;
+
+
 /**
  * An importer is a KF metamodel instance of an OWL 2 specification
  * 
@@ -42,6 +54,7 @@ public class Importer {
 	private MetaConverter metabuilder;
 	private OWLOntology onto;
 	private OWLOntologyManager man;
+	
 	
 	/**
 	 * 
@@ -107,6 +120,76 @@ public class Importer {
         	e.printStackTrace();
 		}
 	}
+	
+	/**
+	 * Starting to loop over ontology axioms to normalise
+	 * Filter axiom types http://owlcs.github.io/owlapi/apidocs_5/org/semanticweb/owlapi/model/AxiomType.html
+	 * 
+	 * @implNote we remove unsupported class expressions not removed by ontology utils dependency
+	 * 
+	 * @param ontology
+	 * @return a normalised ontology to be imported
+	 */
+	public static void normalizeToImport(OWLOntology ontology) {
+		FreshAtoms.resetFreshAtomsEquivalenceAxioms(); // optional; for verification purpose
+
+		OWLOntology copy = Utils.newEmptyOntology();
+		copy.addAxioms(ontology.axioms());
+		
+		OWLOntology unsupported = Utils.newEmptyOntology();
+
+		Stream<OWLAxiom> tBoxAxioms = copy.tboxAxioms(Imports.EXCLUDED);
+		tBoxAxioms.forEach((ax) -> {
+			copy.remove(ax);
+
+			try {
+				OWLClassExpression left = ((OWLSubClassOfAxiom) ax).getSubClass();
+				OWLClassExpression right = ((OWLSubClassOfAxiom) ax).getSuperClass();
+				
+				if ((left.getClassExpressionType() == ClassExpressionType.OBJECT_HAS_VALUE) || 
+							(right.getClassExpressionType() == ClassExpressionType.OBJECT_HAS_VALUE)) {
+						
+						unsupported.addAxiom(ax);
+				} else {
+					copy.addAxioms(NormalizationTools.asSubClassOfAxioms(ax));
+				}
+			}
+			catch (Exception f) {
+					System.out.println("Unsupported axioms:" + ax.toString());
+			}
+		
+		});
+		
+		System.out.println("\nNaive Normalized TBox");
+		//OWLOntology naive = null;
+		OWLOntology naive = Utils.newEmptyOntology();
+		naive = Normalization.normalizeNaive(copy);
+		
+		System.out.println("\n ************List Unsupported ClassExpressions and Axioms in Normalization App\n");
+		// After normalize, copy again the unsupported axioms
+		
+		unsupported.axioms().forEach(System.out::println);
+		
+		naive.addAxioms(unsupported.axioms());
+		
+		// check every axiom of the original ontology is entailed in naive
+		OWLReasoner reasoner = Utils.getHermitReasoner(naive);
+		//assert (ontology.axioms().allMatch(ax -> reasoner.isEntailed(ax)));
+		
+		copy.addAxioms(unsupported.axioms());
+		
+		// check every axiom of naive is entailed in the copy of the original ontology
+		// with extended signature
+		copy.addAxioms(FreshAtoms.getFreshAtomsEquivalenceAxioms());
+		OWLReasoner reasonerBis = Utils.getHermitReasoner(copy);
+		assert (naive.axioms().allMatch(ax -> reasonerBis.isEntailed(ax)));	
+		
+		naive.tboxAxioms(Imports.EXCLUDED).forEach(ax -> System.out.println(Utils.pretty("-- " + ax.toString())));
+		
+		return naive;
+
+	}
+	
 	
 	public Metamodel getKFInstance() {
 		return this.kfimported;
