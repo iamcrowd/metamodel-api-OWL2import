1cad8ceb31d32009b8c6e7138a25daca7b135692
German Braun <gab@gab.gab>
2021-06-04 17:52:45 -0300

    adding test for a complex axioms from people.owl

--- src/test/java/com/gilia/importer/ImporterTest.java
+++ src/test/java/com/gilia/importer/ImporterTest.java
@@ -382,5 +382,22 @@ public class ImporterTest {
         	e.printStackTrace();
     	}
     }
+
+	@Test
+    public void testOnlyImportPeopleasKF() {
+    	try {
+    		String path = new String(ImporterTest.class.getClassLoader().getResource("ontologies/ex-people.owl").toString());
+    		String[] owlfilepath = path.split(":", 2);
+    	  	Importer importer = new Importer(owlfilepath[1],true);
+    	  	Metamodel meta = importer.getKFInstance();
+    	  	OWLOntology onto = importer.getOntology();
+    	  	importer.importNormalisedOntology();
+    	  	System.out.println(importer.toJSON());
+    	  	
+    	}
+    	catch (Exception e){
+        	e.printStackTrace();
+    	}
+    }
     
 }
