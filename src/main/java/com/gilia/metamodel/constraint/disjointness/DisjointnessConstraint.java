83b67e03eb810978c77a299b65e8a8110e5708c8
German Braun <gab@gab.gab>
2021-08-20 15:58:30 -0300

    CNL specs (en) for Disjoint Object types

--- src/main/java/com/gilia/metamodel/constraint/disjointness/DisjointnessConstraint.java
+++ src/main/java/com/gilia/metamodel/constraint/disjointness/DisjointnessConstraint.java
@@ -2,6 +2,12 @@ package com.gilia.metamodel.constraint.disjointness;
 
 import com.gilia.metamodel.constraint.Constraint;
 
+import simplenlg.framework.NLGFactory;
+import simplenlg.lexicon.Lexicon;
+import simplenlg.phrasespec.SPhraseSpec;
+import simplenlg.realiser.english.Realiser;
+
+
 /**
  * Representation of the Disjointness constraint class from the KF Metamodel
  *
@@ -16,4 +22,15 @@ public class DisjointnessConstraint extends Constraint {
     public DisjointnessConstraint(String name) {
         super(name);
     }
+    
+    /**
+     * English verbalisation of Disjointness constraints
+     * @return
+     */
+    public void toCNLen() {
+  	  this.cnl.setSubject(this.name);
+  	  this.cnl.setVerb("is");
+  	  this.cnl.setObject("a Disjointness constraint");
+    }
+    
 }
